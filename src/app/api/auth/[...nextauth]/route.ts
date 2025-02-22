963cc64634ca41d145e9e7ab2eaf235b26676b8a
Cristian <adrian.cristian.st@gmail.com>
2025-02-22 20:05:45 -0300

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,28 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import GoogleProvider from "next-auth/providers/google"
+// import NextAuth from "next-auth"
+// import { PrismaAdapter } from "@next-auth/prisma-adapter"
+// import { PrismaClient } from "@prisma/client"
+// import GoogleProvider from "next-auth/providers/google"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-export const authOptions = {
-  adapter: PrismaAdapter(prisma), // Adicionando o Prisma
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
-    }),
-  ],
+// export const authOptions = {
+//   adapter: PrismaAdapter(prisma), // Adicionando o Prisma
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID!,
+//       clientSecret: process.env.GOOGLE_SECRET!,
+//     }),
+//   ],
 
-  secret: process.env.NEXTAUTH_SECRET, // Definir a chave secreta
+//   secret: process.env.NEXTAUTH_SECRET, // Definir a chave secreta
 
-  callbacks: {
-    async session({ session, user }: { session, user: { id: string } }) {
-      session.user.id = user.id // Adiciona o ID do usuário à sessão
-      return session
-    }
-  }
-}
+//   callbacks: {
+//     async session({ session, user }: { session, user: { id: string } }) {
+//       session.user.id = user.id // Adiciona o ID do usuário à sessão
+//       return session
+//     }
+//   }
+// }
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+// const handler = NextAuth(authOptions)
+// export { handler as GET, handler as POST }
