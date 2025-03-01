d167abdb5bc6897e499524960d4af5d58b84e416
Cristian <adrian.cristian.st@gmail.com>
2025-03-01 15:02:35 -0300

    feat: added insigths from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -40,6 +43,8 @@ export default function RootLayout({
         }
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
